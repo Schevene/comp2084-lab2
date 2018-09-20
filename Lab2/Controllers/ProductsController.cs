4eaf73cba24ef6c0982f7307b128d86951128551
Schevene <Schevene.Singh@MyGeorgian.ca>
2018-09-20 15:50:45 -0400

    lab 2 complete

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Products/Product
+        public ActionResult Details(string ProductName) //matches name of parameter in index
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
